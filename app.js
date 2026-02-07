746ec2391b3bb6966cc576e2984c3719abc5772f
Aditya Singh <adityasingh@bucle.dev>
2026-02-07 11:54:33 +0530

    add end point

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth",require('./src/routes/auth.routes'))
+app.use("/api/tasks",require('./src/routes/task.routes'))
+
 
 
 
